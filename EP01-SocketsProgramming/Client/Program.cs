1537099b6062d7a95f79493f124db0bbd5621613
Simon Nikolaidis <s-nikolaidis@web.de>
2021-11-30 09:01:48 +0100

    SocketServer v.0.1.0

---
+++ EP01-SocketsProgramming/Client/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Press Enter to Connect");
+            Console.ReadKey();
+
+            var endpoint = new IPEndPoint(IPAddress.Loopback, 3000);
+            var socket = new Socket(endpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Connect(endpoint);
+
+            var networkStream = new NetworkStream(socket, true);
+
+            var msg = "Hello World";
+
+            var buffer = System.Text.Encoding.UTF8.GetBytes(msg);
+
+            networkStream.Write(buffer, 0, buffer.Length);
+
+            var response = new byte[1024];
+
+            var bytesRead = networkStream.Read(response, 0, response.Length);
+            var responseStr = System.Text.Encoding.UTF8.GetString(response);
+
+            Console.WriteLine($"Received: {responseStr}");
+
+            Console.ReadKey();
+        }
+    }
+}
